4583c9ea3e880ff4ef31aaf11db31a56c3b316f5
BIKOUTA Guyverna Mon-Mot Désir <bikoutaguyverna@gmail.com>
2026-07-25 06:07:15 +0100

    feat: squelette de 9 fonction deja posé

--- utils.js
+++ utils.js
@@ -1,5 +1,45 @@
 // Le pont JS -> page. À recopier tel quel dans chaque projet S7 à S9.
 const sortie = document.querySelector("#sortie");
 function afficher(html) {
- sortie.innerHTML += html;
+  sortie.innerHTML += html;
+}
+
+function estPair(n) {
+  //verifie si un entier est par
+  let isPair = n % 2 === 0 ? true : false;
+  return isPair;
+}
+
+function maxDeDeux(a, b) {
+  //verifie le plus grand des deux nombre;
+  higher = a - b > 0 ? a : b;
+  return higher
+}
+
+function convertirTemperature(c) {
+  //
+}
+
+function calculerTVA(prix, taux) {
+  //
+}
+
+function inverserChaine(texte) {
+  //
+}
+
+function compterVoyelles(texte) {
+  //
+}
+
+function estPalindrome(texte) {
+  //
+}
+
+function factorielle(n) {
+  //
+}
+
+function swap(var1, var2) {
+  //
 }
